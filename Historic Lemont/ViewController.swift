94875001d23a1bcd196a22c3e1c80aa6490277a6
Amelia Dunkin <ameliardunkin@gmail.com>
2022-12-26 11:55:45 -0600

    Initial Commit

---
+++ Historic Lemont/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Historic Lemont
+//
+//  Created by Ethan Dunkin on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
